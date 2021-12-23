1054dfc814d4623c72d6f58971f34d37f5b90be0
Satyam kant <satyamkant2805@gmail.com>
2021-12-23 21:38:59 +0530

    update

--- Bipartite Graph Check.cpp
+++ Bipartite Graph Check.cpp
@@ -1,4 +1,4 @@
-//Author:- satyam kant//
+// Author:- satyam kant//
 
 #include <bits/stdc++.h>
 
@@ -8,36 +8,36 @@ class Bipartite_Check
 {
     bool bfs_Bipartite_Check(vector<int> adj[], vector<int> &color, int n)
     {
-        //if the graph is disconnected...then this for loop will take care of that
+        // if the graph is disconnected...then this for loop will take care of that
         for (int i = 0; i < n; i++)
         {
-            //the color array will also act as visited array...
+            // the color array will also act as visited array...
             if (color[i] == -1)
             {
-                //creating queue for bfs...
+                // creating queue for bfs...
                 queue<int> qu;
-                //pushing the parent inside the queue first...
+                // pushing the parent inside the queue first...
                 qu.push(i);
-                //starting with color 1 i.e. assigning 1 to color array for current node...
+                // starting with color 1 i.e. assigning 1 to color array for current node...
                 color[i] = 1;
 
                 while (!qu.empty())
                 {
                     int node = qu.front();
                     qu.pop();
-                    //traversing the child of the current node...
+                    // traversing the child of the current node...
                     for (auto it : adj[node])
                     {
                         if (color[it] == -1)
                         {
-                            //assigning opposite color of parent to the child...
+                            // assigning opposite color of parent to the child...
                             color[it] = 1 - color[node];
-                            //since we have assigned color to this child then we will push it in the queue so
-                            //we will assign color to the children of this child...
+                            // since we have assigned color to this child then we will push it in the queue so
+                            // we will assign color to the children of this child...
                             qu.push(it);
                         }
-                        else if (color[it] == color[node]) //color of the child is same as the color of the parent...
-                            return false;                  //i.e. not a bipertite graph...
+                        else if (color[it] == color[node]) // color of the child is same as the color of the parent...
+                            return false;                  // i.e. not a bipertite graph...
                     }
                 }
             }
@@ -51,12 +51,12 @@ class Bipartite_Check
         {
             if (color[it] == -1)
             {
-                //providing opposite color to the child...
+                // providing opposite color to the child...
                 color[it] = 1 - color[node];
                 if (!dfs_Bipartite_Check(adj, color, it))
                     return false;
             }
-            else if (color[it] == color[node]) //color of the child is same as color of the parent
+            else if (color[it] == color[node]) // color of the child is same as color of the parent
                 return false;                  // i.e. same color at adjacent nodes...not bipertite
         }
         return true;
@@ -65,23 +65,23 @@ class Bipartite_Check
 public:
     bool CheckUsing_bfs(vector<int> adj[], int n)
     {
-        //declaring color array...
+        // declaring color array...
         vector<int> color(n + 1, -1);
         return bfs_Bipartite_Check(adj, color, n);
     }
 
     bool CheckUsing_dfs(vector<int> adj[], int n)
     {
-        //declaring color array...
+        // declaring color array...
         vector<int> color(n + 1, -1);
 
-        //if the graph is disconnected...then this for loop will take care of that
+        // if the graph is disconnected...then this for loop will take care of that
         for (int i = 0; i < n; i++)
         {
-            //the color array will also act as visited array...
+            // the color array will also act as visited array...
             if (color[i] == -1)
             {
-                //starting with color 1 and then alternating...
+                // starting with color 1 and then alternating...
                 color[i] = 1;
                 if (!dfs_Bipartite_Check(adj, color, i))
                     return false;
@@ -93,15 +93,15 @@ public:
 
 int main()
 {
-    //n nodes and m edges
+    // n nodes and m edges
     int n, m;
 
     cin >> n >> m;
 
-    //declaring adjacency list...
+    // declaring adjacency list...
     vector<int> adj[n + 1];
 
-    //for input...
+    // for input...
     for (int i = 0; i < m; i++)
     {
         int u, v;
@@ -109,15 +109,15 @@ int main()
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    //declaring object...
+    // declaring object...
     Bipartite_Check check;
 
-    //using bfs...
+    // using bfs...
     if (check.CheckUsing_bfs(adj, n))
         cout << "YES";
     else
         cout << "NO";
-    //using dfs...
+    // using dfs...
     if (check.CheckUsing_dfs(adj, n))
         cout << "\nYES";
     else
